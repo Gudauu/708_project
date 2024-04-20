ab79e483edcaf97cf59be4a0304570f1c5e6c696
Peaker Zhou <142247626+Zhou4truth@users.noreply.github.com>
2024-04-20 16:34:00 -0400

    Update high_cond.m

--- high_cond.m
+++ high_cond.m
@@ -1,7 +1,7 @@
-sizes = [10,100,500,1000, 5000];
+sizes = [10, 100, 500, 1000, 5000];
 matrices = cell(size(sizes,2));
 for i=1:size(sizes,2)
-    matrices{i} = genMatrix(sizes(i), 1e9);
+    matrices{i} = genMatrix(sizes(i), 1e9); % Assume genMatrix is a function defined elsewhere
 end
 
 relresgm = zeros(size(matrices,2),1);
@@ -14,46 +14,46 @@ flags = zeros(size(matrices,2),1);
 
 for i = 1:size(matrices,2)
     b = rand(size(matrices{i},1),1);
-    tic
+    tic;
     [x, flag, relres, iter] = gmres(matrices{i}, b, [], 1e-6, 30);
     timegm(i) = toc;
     relresgm(i) = relres;
     flags(i) = flag;
     itergm(i) = iter(2);
 
-    tic
-    [x, relres, iter] = iterref(matrices{i}, b);
+    tic;
+    [x, relres, iter] = iterref(matrices{i}, b); 
     timelu(i) = toc;
     relreslu(i) = relres(size(relres,2));
     iterlu(i) = iter;
 end
 
 figure(1)
-semilogy(sizes, relreslu);
-title("luir: Relative Residual vs Matrix Size (condition = 1e9)")
-xlabel("Matrix Size")
-ylabel("Relative Residual")
-saveas(gcf,'luir_acc_hcond.png')
-figure(2)
-semilogy(sizes, relresgm);
-title("gmres: Relative Residual vs Matrix Size (condition = 1e9)")
-xlabel("Matrix Size")
-ylabel("Relative Residual")
-saveas(gcf,'gmres_acc_hcond.png')
+semilogy(sizes, relreslu, '-o', 'DisplayName', 'luir');
+hold on; 
+semilogy(sizes, relresgm, '-x', 'DisplayName', 'gmres');
+title("Relative Residual vs Matrix Size (condition = 1e9)");
+xlabel("Matrix Size");
+ylabel("Relative Residual");
+grid on;
+legend('Location', 'northeast');
+hold off; 
+saveas(gcf, 'combined_residuals_hcond.png');
 
 figure(3)
-plot(sizes, iterlu, sizes, itergm);
-title("Iterations vs Matrix Size (condition = 1e9)")
-xlabel("Matrix Size")
-ylabel("Number of Iterations")
-legend("luir", "gmres")
-saveas(gcf,'iter_hcond.png')
+plot(sizes, iterlu, '-o', sizes, itergm, '-o');
+title("Iterations vs Matrix Size (condition = 1e9)");
+xlabel("Matrix Size");
+ylabel("Number of Iterations");
+legend("luir", "gmres", 'Location', 'northwest');
+grid on;
+saveas(gcf, 'iter_hcond.png');
 
 figure(4)
-plot(sizes, timelu, sizes, timegm);
-title("Time vs Matrix Size (condition = 1e9)")
-xlabel("Matrix Size")
-ylabel("Time (seconds)")
-legend("luir", "gmres")
-saveas(gcf,'time_hcond.png')
-
+plot(sizes, timelu, '-o', sizes, timegm, '-o');
+title("Time vs Matrix Size (condition = 1e9)");
+xlabel("Matrix Size");
+ylabel("Time (seconds)");
+legend("luir", "gmres", 'Location', 'northwest');
+grid on;
+saveas(gcf, 'time_hcond.png');
